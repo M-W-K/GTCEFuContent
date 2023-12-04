22ca5549aa607b28e358d429159b6416938728c2
M-W-K <mwk001@icloud.com>
2023-12-03 19:06:11 -0700

    Check compat with GCYM 1.2.7 and GTCEu 2.8.2

--- src/main/java/com/m_w_k/gtcefucontent/GTCEFuContent.java
+++ src/main/java/com/m_w_k/gtcefucontent/GTCEFuContent.java
@@ -27,7 +27,7 @@ public final class GTCEFuContent {
     public static final String NAME = "GregTechCEFuContent";
     public static final String VERSION = "1.4.0";
 
-    public static final String DEP_VERSION_STRING = "required-after:gregtech@[2.8-beta,);" +
+    public static final String DEP_VERSION_STRING = "required-after:gregtech@[2.8.2-beta,);" +
             "required-after:gcym@[1.2.6,);" +
             "required-after:projecte@[1.4.1,);" +
             "required-after:projectex@[1.2.0,);";
