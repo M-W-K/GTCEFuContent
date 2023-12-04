7ec5d0bc232c41b7303b648ec7750fb61bb99ec4
M-W-K <mwk001@icloud.com>
2023-12-03 18:23:18 -0700

    Prep for 1.4 release

--- src/main/java/com/m_w_k/gtcefucontent/GTCEFuContent.java
+++ src/main/java/com/m_w_k/gtcefucontent/GTCEFuContent.java
@@ -25,7 +25,7 @@ public final class GTCEFuContent {
 
     public static final String MODID = "gtcefucontent";
     public static final String NAME = "GregTechCEFuContent";
-    public static final String VERSION = "1.3.0";
+    public static final String VERSION = "1.4.0";
 
     public static final String DEP_VERSION_STRING = "required-after:gregtech@[2.8-beta,);" +
             "required-after:gcym@[1.2.6,);" +
