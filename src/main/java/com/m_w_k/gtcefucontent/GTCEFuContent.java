5a756c555bbb70af80791bd4532000c7536a1ad1
M-W-K <mwk001@icloud.com>
2024-05-12 16:48:06 -0600

    Environment inprovements

--- src/main/java/com/m_w_k/gtcefucontent/GTCEFuContent.java
+++ src/main/java/com/m_w_k/gtcefucontent/GTCEFuContent.java
@@ -25,9 +25,9 @@ import com.m_w_k.gtcefucontent.loaders.recipe.GTCEFuCMiscRecipes;
      dependencies = GTCEFuContent.DEP_VERSION_STRING)
 public final class GTCEFuContent {
 
-    public static final String MODID = "gtcefucontent";
-    public static final String NAME = "GregTechCEFuContent";
-    public static final String VERSION = "1.5.0";
+    public static final String MODID = Tags.MODID;
+    public static final String NAME = Tags.MODNAME;
+    public static final String VERSION = Tags.VERSION;
 
     public static final String DEP_VERSION_STRING = "required-after:gregtech@[2.8.4-beta,);" +
             "required-after:gcym@[1.2.6,);" +
