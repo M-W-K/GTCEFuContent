d04ba4371dd58a92b7244408d5da1227a6175a3a
M-W-K <mwk001@icloud.com>
2024-06-12 13:47:20 -0600

    Escape the GT asset folder

--- src/main/java/com/m_w_k/gtcefucontent/api/render/GTCEFuCTextures.java
+++ src/main/java/com/m_w_k/gtcefucontent/api/render/GTCEFuCTextures.java
@@ -16,44 +16,51 @@ import gregtech.client.renderer.texture.cube.SimpleOverlayRenderer;
 @Mod.EventBusSubscriber(modid = GregicalityMultiblocks.MODID, value = Side.CLIENT)
 public final class GTCEFuCTextures {
 
-    public static SimpleOverlayRenderer INDESTRUCTIBLE_CASING;
-    public static SimpleOverlayRenderer INDESTRUCTIBLE_PIPE_CASING;
-    public static SimpleOverlayRenderer PLASMA_PIPE_CASING;
-    public static SimpleOverlayRenderer THERMOSTABLE_CERAMIC;
-    public static SimpleOverlayRenderer PRESSURE_CASING;
-    public static SimpleOverlayRenderer UNSTABLE_HYPERSTATIC_CASING;
-    public static SimpleOverlayRenderer HYPERSTATIC_CASING;
+    public static SimpleOverlayRenderer INDESTRUCTIBLE_CASING = simple("casings/hardened_casing/indestructible_casing");
+    public static SimpleOverlayRenderer INDESTRUCTIBLE_PIPE_CASING = simple("casings/hardened_casing/indestructible_pipe_casing");
+    public static SimpleOverlayRenderer PLASMA_PIPE_CASING = simple("casings/hardened_casing/plasma_pipe_casing");
+    public static SimpleOverlayRenderer THERMOSTABLE_CERAMIC = simple("casings/standard_casing/thermostable_ceramic");
+    public static SimpleOverlayRenderer PRESSURE_CASING = simple("casings/standard_casing/high_pressure_casing");
+    public static SimpleOverlayRenderer UNSTABLE_HYPERSTATIC_CASING = simple("casings/standard_casing/unstable_hyperstatic_casing");
+    public static SimpleOverlayRenderer HYPERSTATIC_CASING = simple("casings/hardened_casing/hyperstatic_casing");
 
     public static final List<AxisAlignedCubeRenderer> HEU_COMPONENT_EMPTY_OVERLAYS = new ArrayList<>(5);
     public static final List<AxisAlignedCubeRenderer> HEU_COMPONENT_FULL_OVERLAYS = new ArrayList<>(5);
     public static final List<AxisAlignedCubeRenderer> HEU_COMPONENT_ACTIVE_OVERLAYS = new ArrayList<>(5);
 
-    public static final OrientedOverlayRenderer NAQ_REACTOR_OVERLAY =
-            new OrientedOverlayRenderer("generators/naquadah");
+    public static final OrientedOverlayRenderer NAQ_REACTOR_OVERLAY = oriented("generators/naquadah");
+
+    public static final OrientedOverlayRenderer ANTIMATTER_COMPRESSOR_OVERLAY =
+            oriented("machines/antimatter_compressor");
+
 
 
     private GTCEFuCTextures() {}
 
     public static void preInit() {
-        INDESTRUCTIBLE_CASING = new SimpleOverlayRenderer("casings/hardened_casing/indestructible_casing");
-        INDESTRUCTIBLE_PIPE_CASING = new SimpleOverlayRenderer("casings/hardened_casing/indestructible_pipe_casing");
-        PLASMA_PIPE_CASING = new SimpleOverlayRenderer("casings/hardened_casing/plasma_pipe_casing");
-        HYPERSTATIC_CASING = new SimpleOverlayRenderer("casings/hardened_casing/hyperstatic_casing");
-
-        THERMOSTABLE_CERAMIC = new SimpleOverlayRenderer("casings/standard_casing/thermostable_ceramic");
-        PRESSURE_CASING = new SimpleOverlayRenderer("casings/standard_casing/high_pressure_casing");
-        UNSTABLE_HYPERSTATIC_CASING = new SimpleOverlayRenderer("casings/standard_casing/unstable_hyperstatic_casing");
 
         String[] heuComponentNames = new String[] { "endpoint_standard", "endpoint_returning",
                 "pipe_holder_standard", "pipe_holder_conductive", "pipe_holder_expanded" };
 
         for (String heuComponentName : heuComponentNames) {
             HEU_COMPONENT_EMPTY_OVERLAYS
-                    .add(new AxisAlignedCubeRenderer("overlay/machine/heu/" + heuComponentName + "/empty"));
+                    .add(axisAligned("machines/heu/" + heuComponentName + "/empty"));
             HEU_COMPONENT_FULL_OVERLAYS
-                    .add(new AxisAlignedCubeRenderer("overlay/machine/heu/" + heuComponentName + "/full"));
+                    .add(axisAligned("machines/heu/" + heuComponentName + "/full"));
             HEU_COMPONENT_ACTIVE_OVERLAYS
-                    .add(new AxisAlignedCubeRenderer("overlay/machine/heu/" + heuComponentName + "/active"));
+                    .add(axisAligned("machines/heu/" + heuComponentName + "/active"));
         }
+    }
+
+    private static SimpleOverlayRenderer simple(String basePath) {
+        return new SimpleOverlayRenderer("gtcefucontent:" + basePath);
+    }
+
+    private static OrientedOverlayRenderer oriented(String basePath) {
+        return new OrientedOverlayRenderer("gtcefucontent:" + basePath);
+    }
+
+    private static AxisAlignedCubeRenderer axisAligned(String basePath) {
+        return new AxisAlignedCubeRenderer("gtcefucontent:" + basePath);
     }
 }
--- src/main/java/com/m_w_k/gtcefucontent/common/item/armor/GTCEFuCMetaArmor.java
+++ src/main/java/com/m_w_k/gtcefucontent/common/item/armor/GTCEFuCMetaArmor.java
@@ -1,11 +1,14 @@
 package com.m_w_k.gtcefucontent.common.item.armor;
 
+import com.m_w_k.gtcefucontent.api.util.GTCEFuCUtil;
+import gregtech.api.items.metaitem.MetaItem;
 import net.minecraft.inventory.EntityEquipmentSlot;
 
 import com.m_w_k.gtcefucontent.common.item.GTCEFuCMetaItems;
 
 import gregtech.api.items.armor.ArmorMetaItem;
 import gregtech.common.ConfigHolder;
+import net.minecraft.util.ResourceLocation;
 
 public class GTCEFuCMetaArmor extends ArmorMetaItem<ArmorMetaItem<?>.ArmorMetaValueItem> {
 
@@ -17,5 +20,10 @@ public class GTCEFuCMetaArmor extends ArmorMetaItem<ArmorMetaItem<?>.ArmorMetaVa
         GTCEFuCMetaItems.GAS_MASK = addItem(2, "gas_mask").setArmorLogic(new GasMask(2,
                 80_000L * (long) Math.max(1, Math.pow(1, ConfigHolder.tools.voltageTierNightVision - 1)),
                 ConfigHolder.tools.voltageTierNightVision, EntityEquipmentSlot.HEAD));
+    }
+
+    @Override
+    public ResourceLocation createItemModelPath(ArmorMetaItem<?>.ArmorMetaValueItem metaValueItem, String postfix) {
+        return GTCEFuCUtil.gtcefucId(formatModelPath(metaValueItem) + postfix);
     }
 }
